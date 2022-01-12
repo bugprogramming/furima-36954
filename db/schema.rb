428049a816b0e576ca0ec5ef4d49921ec7716db9
bugprogramming <mbotsurinamu135513@icloud.com>
2022-01-12 20:47:18 +0900

    deviseに関するビューファイルの配置

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 ActiveRecord::Schema.define(version: 2022_01_10_110702) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "birthday", null: false
+    t.string "last_name_jpn", null: false
+    t.string "first_name_jpn", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
