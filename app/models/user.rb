07f58fdb3213060929a343a1c9c9fd2c2dcb2b45
bugprogramming <mbotsurinamu135513@icloud.com>
2022-01-14 19:40:45 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ has_many :imagess
+ has_many :purchases
+       
+ validates :birthday, presence: true
+ validates :last_name_jpn, presence: true
+ validates :first_name_jpn, presence: true
+ validates :last_name, presence: true
+ validates :first_name_name, presence: true  
+ validates :nickname, presence: true 
+ validates :email, presence: true
 end
